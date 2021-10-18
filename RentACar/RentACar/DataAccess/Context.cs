0807f5e7b9d1da546caf972034d924fc48a0690f
Artur Solod <solod1467@gmail.com>
2021-10-18 14:46:54 +0300

    DBConnection commit

--- RentACar/RentACar/DataAccess/Context.cs
+++ RentACar/RentACar/DataAccess/Context.cs
@@ -1,16 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RentACar.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RentACar.DataAccess
 {
     public class Context: DbContext
     {
         public Context(DbContextOptions options) : base(options) {}
-        public DbSet<Admin> Admins { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Car> Cars { get; set; }
