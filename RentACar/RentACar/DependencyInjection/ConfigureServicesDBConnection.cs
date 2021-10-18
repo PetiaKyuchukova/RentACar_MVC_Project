0be92c53a70237d356696d10e2232d5d4f7cf9d1
Artur Solod <solod1467@gmail.com>
2021-10-18 14:47:59 +0300

    connection added

--- RentACar/RentACar/DependencyInjection/ConfigureServicesDBConnection.cs
+++ RentACar/RentACar/DependencyInjection/ConfigureServicesDBConnection.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RentACar.DataAccess;
 
 namespace RentACar.DependencyInjection
 {
-    public class ConfigureServicesDBConnection
+    public static class ConfigureServicesDBConnection
     {
+        public static IServiceCollection AddDBConnection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<Context>(
+                options => options.UseNpgsql(configuration["DBConnection:ConnectionString"]));
+
+            return services;
+        }
     }
 }
